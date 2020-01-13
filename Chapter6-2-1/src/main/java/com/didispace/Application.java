f0308d1d901c707df0176a4f944e4d7996bffc77
gzz <gzz@163.com>
2020-01-14 00:11:59 +0800

    format

--- Chapter6-2-1/src/main/java/com/didispace/Application.java
+++ Chapter6-2-1/src/main/java/com/didispace/Application.java
@@ -3,7 +3,6 @@ package com.didispace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
- 
 @SpringBootApplication
 public class Application {
 
